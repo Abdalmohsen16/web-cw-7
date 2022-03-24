939725d0a47d04954a99dcede87aaf3f12bfa30f
Abdalmohsen16 <100500608+Abdalmohsen16@users.noreply.github.com>
2022-03-24 17:40:07 +0300

    done

--- Random/script.js
+++ Random/script.js
@@ -1,3 +1,22 @@
 // project أنشئ كائن
 
+const project = {
+    type: 'accounts',
+    users: 100,
+    biography() {
+        return "This is my project";
+    },
+}
 // randomProperty أنشئ دالة
+
+function randomProperty(obj) {
+    const keys = Object.keys(obj)
+
+    const random = Math.floor(Math.random() * keys.length);
+
+    console.log(keys[random]);
+    console.log(obj[`${keys[random]}`])
+}
+
+
+randomProperty(project)
